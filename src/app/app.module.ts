aca4c025c1bef1d410b1ca34898b537068e8c1b2
Yevgeny Levin <devlev1980@gmail.com>
2020-12-26 19:37:49 +0200

    remove not using

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {AboutComponent} from './components/about/about.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from "./shared/material.module";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {ErrorHandlerInterceptor} from "./interceptors/error-handler.interceptor";
-import { SpinnerComponent } from './shared-UI/spinner/spinner.component';
-import { LockInputComponent } from './components/lock-input/lock-input.component';
-import {ComplexFormControlModule} from "./modules/complex-form-control/complex-form-control.module";
+import {MaterialModule} from './shared/material.module';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorHandlerInterceptor} from './interceptors/error-handler.interceptor';
+import {SpinnerComponent} from './shared-UI/spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +23,6 @@ import {ComplexFormControlModule} from "./modules/complex-form-control/complex-f
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    ComplexFormControlModule,
     HttpClientModule
   ],
   providers: [{
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,35 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatTreeModule} from "@angular/material/tree";
-import {MatButtonModule} from "@angular/material/button";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatSortModule} from "@angular/material/sort";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatTableModule} from "@angular/material/table";
-import {MatStepperModule} from "@angular/material/stepper";
-import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {MatSliderModule} from "@angular/material/slider";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatSelectModule} from "@angular/material/select";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatCardModule} from "@angular/material/card";
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatListModule} from "@angular/material/list";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import {MatInputModule} from "@angular/material/input";
-import {MatIconModule} from "@angular/material/icon";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSortModule} from '@angular/material/sort';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 const MATERIAL_MODULES = [
   MatTreeModule,
@@ -62,7 +62,7 @@ const MATERIAL_MODULES = [
   MatAutocompleteModule,
   MatInputModule,
   MatIconModule
-]
+];
 
 
 @NgModule({
@@ -73,4 +73,5 @@ const MATERIAL_MODULES = [
   ],
   exports: [MATERIAL_MODULES]
 })
-export class MaterialModule { }
+export class MaterialModule {
+}
